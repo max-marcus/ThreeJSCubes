34ff41d1306d7a33ad6a032d08f29e0888512630
listentomm <maxmarcususc@gmail.com>
2017-05-05 17:23:56 -0700

    cube rendering function

--- js/main.js
+++ js/main.js
@@ -15,3 +15,24 @@ var isMouseDown = false,
     radius = 1600,
     count = 0;
 
+// CREATE CUBES
+function createGeometry() {
+  var geometry = new THREE.BoxGeometry(100, 100, 100);
+  var object = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
+    color: Math.random() * 0xffffff,
+    opacity: 0.75,
+    map: cubeTexture,
+  }));
+  object.position.x = Math.random() * 800 - 500;
+  object.position.y = Math.random() * 800 - 600;
+  object.position.z = Math.random() * 800 -700;
+  object.scale.x = Math.random() * 2 + 1;
+  object.scale.y = Math.random() * 2 + 1;
+  object.scale.z = Math.random() * 2 + 1;
+  object.rotation.x = Math.random() * 2 * Math.PI;
+  object.rotation.y = Math.random() * 2 * Math.PI;
+  object.rotation.z = Math.random() * 2 * Math.PI;
+
+  return object;
+};
+
